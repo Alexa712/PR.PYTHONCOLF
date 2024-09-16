d42a9b55b5736eaedd4e7423e48ad01f927d40f0
Alexandra _Correa <yacorrea@colfondos.com.co>
2024-09-16 12:21:10 -0500

    estructura de datos

---
+++ estructurasDatos.py
@@ -0,0 +1,18 @@
+#Tuplas y listas
+
+#Tuplas: No son modificables = inmutables 'constante'
+#Listas: Se permite modificar = mutables 'variable'
+
+#tuplas caracteristica ()
+
+tupla = ('David', 'Billy', 'Adriana', 'Daniel')
+print(tupla[0]) #solo permite parametros de lectura
+
+#listas
+
+lista = ['David', 'Billy', 'Adriana', 'Daniel']
+lista2 = ['Milton', 'Jose']
+lista.extend(lista2)
+
+print (lista)
+
