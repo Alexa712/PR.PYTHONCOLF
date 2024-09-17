2344b1cb6739591fd76effdbfcfa3858a170783b
Alexandra _Correa <yacorrea@colfondos.com.co>
2024-09-17 12:45:27 -0500

    CICLO FOR IN

---
+++ diccionariosCiclos.py
@@ -0,0 +1,40 @@
+#Diccionarios en python simples.
+
+lista =[
+    {
+    'nombre': 'David',
+    'apeliido': 'Martinez',
+    'edad': 30,
+    'fechaNacimiento': '30/07/1994',
+    'telefono': [
+        '3185551972',
+        '3146651234'
+    ]
+    },
+    {
+    'nombre': 'Billy',
+    'apellido': 'Lozano',
+    'edad': 30,
+    'fechaNacimiento': '30/07/1994',
+       'telefono': [
+        '3185551972',
+        '3146651234'
+    ]
+    },
+    {
+    'nombre': 'Alexandra',
+    'apellido': 'Correa',
+    'edad': 30,
+    'fechaNacimiento': '30/07/1994',
+       'telefono': [
+        '3185551972',
+        '3146651234'
+    ]
+    },
+]
+
+#ciclo for
+
+for i in lista:
+    print(i['nombre'])
+
